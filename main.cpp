62e4cd39cb25ce64ae5459f2ce7781ec97311511
GreaoZaochen <77674075+Zao-chen@users.noreply.github.com>
2026-03-21 18:01:08 +0800

    feat: 添加角色导入和导出功能，优化角色配置加载 (#18)

--- main.cpp
+++ main.cpp
@@ -9,54 +9,50 @@
 #include <QSystemTrayIcon>
 
 #include <QDir>
-#include <QMessageBox>
-#include <qmessagebox.h>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
 
-    QCoreApplication::setApplicationName("ZcChat2");
-    QCoreApplication::setOrganizationName("MyOrganization");
-    qInfo() << "Debugging Output";
+  QCoreApplication::setApplicationName("ZcChat2");
+  QCoreApplication::setOrganizationName("MyOrganization");
+  qInfo() << "Debugging Output";
 
-    /*窗口创建*/
-    Dialog dialogWin;
-    dialogWin.show();
-    Tachie tachieWin;
-    tachieWin.show();
-    MainWindow *settings = nullptr;
+  /*窗口创建*/
+  Dialog dialogWin;
+  dialogWin.show();
+  Tachie tachieWin;
+  tachieWin.show();
+  MainWindow *settings = nullptr;
 
-    /*一些绑定*/
-    //对话框的开启和关闭
-    QObject::connect(&tachieWin, &Tachie::requestToggleVisible, &dialogWin, &Dialog::ToggleVisible);
-    //修改立绘图片
-    QObject::connect(&dialogWin, &Dialog::requestSetCharTachie, &tachieWin, &Tachie::SetTachieImg);
+  /*一些绑定*/
+  // 对话框的开启和关闭
+  QObject::connect(&tachieWin, &Tachie::requestToggleVisible, &dialogWin,
+                   &Dialog::ToggleVisible);
+  // 修改立绘图片
+  QObject::connect(&dialogWin, &Dialog::requestSetCharTachie, &tachieWin,
+                   &Tachie::SetTachieImg);
 
-    /*托盘*/
-    QSystemTrayIcon tray;
-    tray.setIcon(QIcon(":/res/img/logo/logo.png"));
-    tray.setToolTip("ZcChat2");
-    ElaMenu trayMenu;
-    QAction *actionSettings = trayMenu.addAction("设置");
-    QAction *actionQuit = trayMenu.addAction("退出");
-    tray.setContextMenu(&trayMenu);
-    tray.show();
-    //设置界面懒加载
-    QObject::connect(actionSettings, &QAction::triggered,
-                     [&]()
-                     {
-                         if (!settings)
-                         {
-                             eApp->init();
-                             settings = new MainWindow(&dialogWin, &tachieWin, &dialogWin);
-                         }
-                         settings->show();
-                         settings->raise();
-                         settings->activateWindow();
-                     });
-    //退出程序
-    QObject::connect(actionQuit, &QAction::triggered, &a, &QApplication::quit);
+  /*托盘*/
+  QSystemTrayIcon tray;
+  tray.setIcon(QIcon(":/res/img/logo/logo.png"));
+  tray.setToolTip("ZcChat2");
+  ElaMenu trayMenu;
+  QAction *actionSettings = trayMenu.addAction("设置");
+  QAction *actionQuit = trayMenu.addAction("退出");
+  tray.setContextMenu(&trayMenu);
+  tray.show();
+  // 设置界面懒加载
+  QObject::connect(actionSettings, &QAction::triggered, [&]() {
+    if (!settings) {
+      eApp->init();
+      settings = new MainWindow(&dialogWin, &tachieWin, &dialogWin);
+    }
+    settings->show();
+    settings->raise();
+    settings->activateWindow();
+  });
+  // 退出程序
+  QObject::connect(actionQuit, &QAction::triggered, &a, &QApplication::quit);
 
-    return a.exec();
+  return a.exec();
 }
